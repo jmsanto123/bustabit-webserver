caca1f89e6317d8d75b1d8ea0201f5c846e0cff9
Rakesh <rakesh@moneypot.com>
2015-06-10 18:29:13 -0500

    Compiled Version

---
+++ build/scripts/components/ChartControls.js
@@ -0,0 +1,35 @@
+define([
+    'react',
+    'components/Chart',
+    'components/ControlsSelector'
+], function(
+    React,
+    ChartClass,
+    ControlsSelectorClass
+) {
+    var D = React.DOM;
+
+    var Chart =  React.createFactory(ChartClass);
+    var ControlsSelector = React.createFactory(ControlsSelectorClass);
+
+    return React.createClass({
+        displayName: 'Chart-Controls',
+
+        propTypes: {
+            isMobileOrSmall: React.PropTypes.bool.isRequired
+        },
+
+        render: function() {
+            return D.div({ id: 'chart-controls-inner-container' },
+                D.div({ id: 'chart-container' },
+                    Chart()
+                ),
+                D.div({ id: 'controls-container' },
+                    ControlsSelector({
+                        isMobileOrSmall: this.props.isMobileOrSmall
+                    })
+                )
+            );
+        }
+    });
+});
