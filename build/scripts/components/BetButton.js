be0edc906d94ca14ea593ebec5050cac567bd6fd
Rakesh <rakesh@moneypot.com>
2015-06-17 16:19:28 -0500

    Compiled Version

---
+++ build/scripts/components/BetButton.js
@@ -0,0 +1,183 @@
+define([
+    'react',
+    'game-logic/clib',
+    'constants/AppConstants',
+    'components/Payout',
+    'game-logic/engine'
+], function(
+    React,
+    Clib,
+    AppConstants,
+    PayoutClass,
+    Engine
+){
+
+    var D = React.DOM;
+    var Payout = React.createFactory(PayoutClass);
+
+    return React.createClass({
+        displayName: 'BetButton',
+
+        propTypes: {
+            engine: React.PropTypes.object.isRequired,
+            invalidBet: React.PropTypes.func.isRequired,
+            placeBet: React.PropTypes.func.isRequired,
+            cancelBet: React.PropTypes.func.isRequired,
+            cashOut: React.PropTypes.func.isRequired,
+            isMobileOrSmall: React.PropTypes.bool.isRequired
+        },
+
+        getInitialState: function() {
+            return {
+                initialDisable: true
+            }
+        },
+
+        componentDidMount: function() {
+            this._initialDisableTimeout();
+            Engine.on({
+                game_crash: this._onGameCrash
+            });
+        },
+
+        componentWillUnmount: function() {
+            Engine.off({
+                game_crash: this._onGameCrash
+            });
+        },
+
+        _onGameCrash: function() {
+            this.setState({ initialDisable: true });
+            this._initialDisableTimeout();
+        },
+
+        _initialDisableTimeout: function() {
+            var self = this;
+            setTimeout(function() {
+                if(self.isMounted())
+                    self.setState({ initialDisable: false });
+            }, AppConstants.BetButton.INITIAL_DISABLE_TIME);
+        },
+
+        //Returns the button to cancel the bet or the message of sending bet
+        //_getSendingBet: function () {
+        //    var cancel;
+        //    if (this.props.engine.gameState !== 'STARTING')
+        //        cancel = D.a({ onClick: this.props.cancelBet }, 'cancel');
+        //
+        //    return D.span(null, 'Sending bet...', cancel);
+        //},
+
+        _cashOut: function () {
+            this.props.cashOut();
+            this.setState({ initialDisable: true });
+            this._initialDisableTimeout();
+        },
+
+        render: function() {
+            var self = this;
+
+            //The user is currently playing
+            var currentPlay = this.props.engine.currentPlay();
+
+            //There is a bet in progress
+            var betting = this.props.engine.isBetting();
+
+            /**
+             * The button is able to bet:
+             *  the game is in progress and there isn't a bet in progress
+             */
+            var ableToBet;
+            if (this.props.betting)
+                ableToBet = false;
+            else
+                ableToBet = !(this.props.engine.gameState === 'IN_PROGRESS' && currentPlay && currentPlay.bet && !currentPlay.stopped_at); //TODO: Document this if and maybe reduce
+
+
+            // Able to bet, or is already betting
+            var ableToBetOrBetting = ableToBet || this.props.betting;
+
+            var invalidBet = this.props.invalidBet();
+
+
+            var btnClasses, btnContent = [], onClickFun = null, onMouseDownFun = null, onMouseUpFun = null;
+            btnClasses = 'bet-button';
+
+            if(ableToBetOrBetting) {
+                //Betting
+                if(betting) {
+                    btnClasses += ' disable';
+
+                    //Can cancel
+                    if (this.props.engine.gameState !== 'STARTING') {
+                        btnContent.push(D.span({ key: 'bc-0'}, this.props.isMobileOrSmall? '' : 'Betting...'), D.a({ className: 'cancel', key: 'bc-1' }, ' (Cancel)'));
+                        onClickFun = this.props.cancelBet;
+                        btnClasses += ' cancel';
+                    } else {
+                        btnContent.push(D.span({ key: 'bc-0'}, 'Betting...'));
+                    }
+
+                    //Initial disable
+                } else if(this.state.initialDisable) {
+                    btnContent.push(D.span({ key: 'bc-2' }, this.props.isMobileOrSmall? 'Bet' : 'Place bet'));
+                    btnClasses += ' disable unselect';
+
+                    //Able to bet
+                } else if(ableToBet) {
+
+                    //Invalid bet
+                    if(invalidBet) {
+                        //btnContent.push(D.span({ key: 'bc-3' }, invalidBet));
+                        btnContent.push(D.span({ key: 'bc-3' }, this.props.isMobileOrSmall? 'Bet' : 'Place bet'));
+                        btnClasses += ' invalid-bet unselect';
+
+                    //Placing bet
+                    } else if(this.props.engine.placingBet) {
+                        btnContent.push(D.span({ key: 'bc-4' }, this.props.isMobileOrSmall? 'Bet' : 'Place bet'));
+                        btnClasses += ' disable unselect';
+
+                    //Able to bet
+                    } else {
+                        btnContent.push(D.span({ key: 'bc-5' }, this.props.isMobileOrSmall? 'Bet' : 'Place bet'));
+                        btnClasses += ' ';
+                        onClickFun = self.props.placeBet;
+                    }
+
+                    //User is cashing out
+                } else {
+                    console.error('Not defined state in controls');
+                }
+
+            //The user is playing
+            } else {
+                btnContent.push(
+                    D.div({ className: 'btn-content', key: 'bc-6' },
+                        D.span({ className: 'cashout-cont' }, 'Cash out'),
+                        D.span({ className: 'cashout-amount-cont' },
+                            D.span(null, '@ '),
+                            Payout({ engine: this.props.engine }),
+                            D.span(null, 'bits')
+                        )
+                    )
+                );
+
+                //Cashing out
+                if (this.props.engine.cashingOut) {
+                    btnClasses += ' disable';
+
+                //Able to cash out
+                } else {
+                    btnClasses += ' cashout';
+                    onMouseDownFun = this._cashOut;
+                }
+            }
+
+            return D.div({ className: 'bet-button-container', onClick: onClickFun, onMouseDown: onMouseDownFun, onMouseUp: onMouseUpFun },
+                D.button({ className: btnClasses },
+                    btnContent
+                )
+            );
+        }
+    });
+
+});
---
+++ build/scripts/components/Game.js
@@ -0,0 +1,175 @@
+/**
+ * This view acts as a wrapper for all the other views in the game
+ * it is subscribed to changes in EngineVirtualStore but it only
+ * listen to connection changes so every view should subscribe to
+ * EngineVirtualStore independently.
+ */
+define([
+    'react',
+    'components/TopBar',
+    'components/ChartControls',
+    'components/TabsSelector',
+    'components/Players',
+    'components/BetBar',
+    'components/SpaceWrap',
+    'game-logic/engine',
+    'game-logic/clib'
+], function(
+    React,
+    TopBarClass,
+    ChartControlsClass,
+    TabsSelectorClass,
+    PlayersClass,
+    BetBarClass,
+    SpaceWrapClass,
+    Engine,
+    Clib
+){
+    var TopBar = React.createFactory(TopBarClass);
+    var SpaceWrap = React.createFactory(SpaceWrapClass);
+    var ChartControls = React.createFactory(ChartControlsClass);
+    var TabsSelector = React.createFactory(TabsSelectorClass);
+    var Players = React.createFactory(PlayersClass);
+    var BetBar = React.createFactory(BetBarClass);
+
+    var D = React.DOM;
+
+    return React.createClass({
+        displayName: 'Game',
+
+        getInitialState: function () {
+            return {
+                isConnected: Engine.isConnected,
+                showMessage: true,
+                isMobileOrSmall: Clib.isMobileOrSmall() //bool
+            }
+        },
+
+        componentDidMount: function() {
+            Engine.on({
+                'connected': this._onChange,
+                'disconnected': this._onChange
+            });
+
+            window.addEventListener("resize", this._onWindowResize);
+        },
+
+        componentWillUnmount: function() {
+            Engine.off({
+                'connected': this._onChange,
+                'disconnected': this._onChange
+            });
+        },
+
+        _onChange: function() {
+            if(this.state.isConnected != Engine.isConnected)
+                this.setState({ isConnected: Engine.isConnected });
+        },
+
+        _onWindowResize: function() {
+            var isMobileOrSmall = Clib.isMobileOrSmall();
+            if(this.state.isMobileOrSmall !== isMobileOrSmall)
+                this.setState({ isMobileOrSmall: isMobileOrSmall });
+        },
+
+        _hideMessage: function() {
+            this.setState({ showMessage: false });
+        },
+
+        render: function() {
+            if (!this.state.isConnected)
+                return D.p(null, 'Connecting to server..');
+
+            var messageContainer;
+            if(USER_MESSAGE && this.state.showMessage) {
+
+                var messageContent, messageClass, containerClass = 'show-message';
+                switch(USER_MESSAGE.type) {
+                    case 'error':
+                        messageContent = D.span(null,
+                                            D.span(null, USER_MESSAGE.text)
+                        );
+                        messageClass = 'error';
+                        break;
+                    case 'newUser':
+                        messageContent = D.span(null,
+                                            D.span(null, "Welcome to bustabit.com, to start you have 2 free bits, bits you can "),
+                                            D.a({ href: "/request" }, "request"),
+                                            D.span(null, " them here or you can just watch the current games... have fun :D")
+                        );
+                        messageClass = 'new-user';
+                        break;
+                    case 'received':
+                        messageContent = D.span(null,
+                            D.span(null, "Congratulations you have been credited " +  USER_MESSAGE.qty +  " free bits. Have fun!")
+                        );
+                        messageClass = 'received';
+                        break;
+                    case 'advice':
+                        messageContent = D.span(null,
+                            D.span(null, USER_MESSAGE.advice)
+                        );
+                        messageClass = 'advice';
+                        break;
+                    case 'collect':
+                        messageContent = D.span(null,
+                            D.a({ href: '/request' }, 'Collect your two free bits!')
+                        );
+                        messageClass = 'collect';
+                        break;
+                    default:
+                        messageContent = null;
+                        messageClass = 'hide';
+                        containerClass = '';
+                }
+
+                messageContainer = D.div({ id: 'game-message-container', className: messageClass },
+                    messageContent,
+                    D.a({ className: 'close-message', onClick: this._hideMessage }, D.i({ className: 'fa fa-times' }))
+                )
+            } else {
+                messageContainer = null;
+                containerClass = '';
+            }
+
+            var rightContainer = !this.state.isMobileOrSmall?
+                D.div({ id: 'game-right-container' },
+                    Players(),
+                    BetBar()
+                ) : null;
+
+            return D.div({ id: 'game-inner-container' },
+
+                TopBar({
+                    isMobileOrSmall: this.state.isMobileOrSmall
+                }),
+
+                SpaceWrap(),
+
+                messageContainer,
+
+                D.div({ id: 'game-playable-container', className: containerClass },
+                    D.div({ id: 'game-left-container', className: this.state.isMobileOrSmall? ' small-window' : '' },
+                        D.div({ id: 'chart-controls-row' },
+                            D.div({ id: 'chart-controls-col' },
+                                ChartControls({
+                                    isMobileOrSmall: this.state.isMobileOrSmall
+                                })
+                            )
+
+                        ),
+                        D.div({ id: 'tabs-controls-row' },
+                            D.div({ id: 'tabs-controls-col' },
+                                TabsSelector()
+                            )
+                        )
+
+                    ),
+                    rightContainer
+                )
+
+            );
+        }
+    });
+
+});
